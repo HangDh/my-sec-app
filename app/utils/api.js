923ff001aea856dede3f59cf4b2f5a33d9d3cca1
HangDh <hangiedh@gmail.com>
2022-09-06 14:29:04 +0200

    Working result page

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,3 @@
-import { Profiler } from "react";
-
 export function fetchPopularRepos(language) {
   const endpoint = window.encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories&per_page=50`
